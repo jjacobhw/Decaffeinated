61e6ceb1e485c788bbf2dad1f76a0d00f297f240
jacob <jacobhwei@outlook.com>
2025-09-15 13:29:30 -0700

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <main>hello</main>
+      <main>Decaffinated</main>
+      <h1>It's time to ditch the all-nighters and caffeine</h1>
       <Link href="/users">
         Users
       </Link>
